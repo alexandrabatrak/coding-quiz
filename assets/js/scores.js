fc471852e662601a116af6e14832624da033a99e
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-01-19 04:17:32 +0000

    Initial file add

---
+++ assets/js/scores.js
@@ -0,0 +1,8 @@
+export function scores() {
+  let finalScore = document.getElementById('final-score');
+  let initials = document.getElementById('initials');
+
+  let score = 0;
+
+  let highScores = [{ score: score, initials: initials }];
+}
