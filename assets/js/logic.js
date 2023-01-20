8c797345462554ab5229245867b51e25610286e2
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-01-20 00:18:41 +0000

    Add sounds, timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -11,8 +11,29 @@ const endScreen = document.getElementById('end-screen');
 let finalScore = document.getElementById('final-score');
 let feedback = document.getElementById('feedback');
 
-let score = 0;
+let timeSpan = document.getElementById('time');
+let timer = document.querySelector('.timer');
+let time;
+let countdownTimer;
+function startTimer() {
+  countdownTimer = setInterval(() => {
+    time--;
+    timeSpan.innerHTML = time;
+    timer.classList.add('show');
+    if (time <= 0) {
+      setTimeout(() => {
+        endQuiz();
+        let title = document.querySelector('#end-screen p');
+        let subtitle = document.createElement('span');
+        subtitle.setAttribute('class', 'subtitle');
+        subtitle.textContent = `Time is up! `;
+        endScreen.insertBefore(subtitle, title);
+      }, 500);
+    }
+  }, 1000);
+}
 
+let score = 0;
 // start quiz
 startBtn.addEventListener('click', startQuiz);
 
@@ -51,15 +72,21 @@ function processAnswer(e) {
   let correctAnswer = question.answers[question.correct];
   if (selectedAnswer === correctAnswer) {
     listOption.classList.add('correct');
+    var correctSound = new Audio();
+    correctSound.src = './assets/sfx/correct.wav';
+    correctSound.play();
     feedback.classList.add('show');
     feedback.innerHTML = 'Correct!';
     // increment score value
     score++;
   } else {
     listOption.classList.add('wrong');
+    var wrongSound = new Audio();
+    wrongSound.src = './assets/sfx/incorrect.wav';
+    wrongSound.play();
     feedback.classList.add('show');
     feedback.innerHTML = 'Wrong!';
-    // timer -10s
+    time -= 10;
   }
   listOptions.forEach((listOption) => {
     listOption.removeEventListener('click', processAnswer);
@@ -68,7 +95,9 @@ function processAnswer(e) {
     feedback.classList.remove('show');
     if (questionIndex === questionsList.length - 1) {
       // end of the quiz
-      endQuiz();
+      setTimeout(() => {
+        endQuiz();
+      }, 500);
     } else {
       questionIndex++;
       questionShow();
@@ -82,10 +111,15 @@ function startQuiz() {
   startScreen.classList.add('hide');
   questionsDiv.classList.remove('hide');
   questionShow();
+  time = 100;
+  // startTimer();
 }
 
 function endQuiz() {
   // end of the quiz
+  clearInterval(countdownTimer);
+  timer.classList.remove('show');
+  timeSpan.innerHTML = 0;
   questionsDiv.classList.add('hide');
   endScreen.classList.remove('hide');
   finalScore.textContent = score;
